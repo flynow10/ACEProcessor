9ce0752c75dab974fe484252738ca7e8e7693cb5
Nate Wagner <nate.o.wagner@gmail.com>
2024-12-08 19:47:25 -0500

    feat: replace clock key with clock50

--- Assembler/Test/vga-test/simple-test.s
+++ Assembler/Test/vga-test/simple-test.s
@@ -3,4 +3,20 @@ main:
   lui t1, 0x48fff
   addi t1, t1, 0xfff
   sw t1, 0(t0)
+  addi t0, t0, 1
+  lui t1, 0x45fff
+  addi t1, t1, 0xfff
+  sw t1, 0(t0)
+  addi t0, t0, 1
+  lui t1, 0x4cfff
+  addi t1, t1, 0xfff
+  sw t1, 0(t0)
+  addi t0, t0, 1
+  lui t1, 0x4cfff
+  addi t1, t1, 0xfff
+  sw t1, 0(t0)
+  addi t0, t0, 1
+  lui t1, 0x4ffff
+  addi t1, t1, 0xfff
+  sw t1, 0(t0)
   hlt
