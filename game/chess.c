b0226d655f1bbaf85d0f187051f82c077810a36f
Nate Wagner <nate.o.wagner@gmail.com>
2024-12-12 18:51:43 -0500

    feat: red test

--- game/chess.c
+++ game/chess.c
@@ -9,18 +9,18 @@ int perft(Board *board, int depth);
 
 int main()
 {
-  int color = 0;
+  int color = 0xff0000;
   while (1)
   {
     reset();
     for (int i = 33; i < 127; i++)
     {
       printColorChar((char)(i), color);
-      color++;
-      if (color >= 0xffffff)
-      {
-        color = 0;
-      }
+      // color++;
+      // if (color >= 0xffffff)
+      // {
+      //   color = 0;
+      // }
     }
   }
   return 0;
