bed95eaf8a9865c71de937bc71e5aa5a2602f394
Nate Wagner <nate.o.wagner@gmail.com>
2024-12-12 21:20:13 -0500

    feat: re implement fibonacci

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -5,28 +5,24 @@
 
 int main()
 {
-    int val1 = 231;
-    printInt(val1);
+    int val1 = 0;
+    int val2 = 1;
+    int val3 = 0;
+
     while (1)
     {
+        for (int i = 0; i < 10; i++)
+        {
+            val3 = val1 + val2;
+            printValue(val3);
+            newLine();
+            val1 = val2 + val3;
+            printValue(val1);
+            newLine();
+            val2 = val1 + val3;
+            printValue(val2);
+            newLine();
+        }
+        reset();
     }
-    // int val2 = 1;
-    // int val3 = 0;
-
-    // while (1)
-    // {
-    //     for (int i = 0; i < 10; i++)
-    //     {
-    //         val3 = val1 + val2;
-    //         printValue(val3);
-    //         newLine();
-    //         val1 = val2 + val3;
-    //         printValue(val1);
-    //         newLine();
-    //         val2 = val1 + val3;
-    //         printValue(val2);
-    //         newLine();
-    //     }
-    //     reset();
-    // }
 }
