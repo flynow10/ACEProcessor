f03c600b66f3e4922cf028c303fe20fd3e2f1c79
Nate Wagner <nate.o.wagner@gmail.com>
2024-12-12 23:22:25 -0500

    feat: compile fun text program

--- fun-text/fun-text.c
+++ fun-text/fun-text.c
@@ -4,13 +4,14 @@ int main()
 {
     int color = 0;
     char *string = "Hello Dr. Jamieson";
-    while (1) 
+    while (1)
     {
         printString(string, color);
         reset();
-        for (int i = 0; i < 0xffffff; i++) 
+        for (int i = 0; i < 0xffffff; i++)
         {
             color++;
         }
     }
+    return 0;
 }
