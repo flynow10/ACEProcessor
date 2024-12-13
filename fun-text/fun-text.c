0200e4d119a69cba118c302f2b61024b7f530236
Nate Wagner <nate.o.wagner@gmail.com>
2024-12-12 23:27:55 -0500

    feat: update color on each char

--- fun-text/fun-text.c
+++ fun-text/fun-text.c
@@ -1,18 +1,23 @@
 #include "print.h"
 
+#include <string.h>
+
 int main()
 {
     int color = 0;
     char *string = "Hello Dr. Jamieson";
     while (1)
     {
-        printString(string, color);
-        reset();
-        color++;
-        if (color > 0xffffff)
+        for (int i = 0; i < strlen(string); i++)
         {
-            color = 0;
+            printColorChar(string[i], color);
+            color++;
+            if (color > 0xffffff)
+            {
+                color = 0;
+            }
         }
+        reset();
     }
     return 0;
 }
