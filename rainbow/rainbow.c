a07e0c3d65a31c26d9a640e78ead548b1861128d
Nate Wagner <nate.o.wagner@gmail.com>
2025-01-29 11:22:35 -0500

    feat: multiple rounds

--- rainbow/rainbow.c
+++ rainbow/rainbow.c
@@ -1,29 +1,33 @@
 #include "print.h"
 
-int getColor(int x, int y);
+int getColor(int x, int y, int round);
 
 static int WIDTH = 80;
 static int HEIGHT = 60;
 
 int main()
 {
-  for (int row = 0; row < HEIGHT; row++)
+  for (int round = 0; round < 255; round++)
   {
-    for (int col = 0; col < WIDTH; col++)
+    for (int row = 0; row < HEIGHT; row++)
     {
-      printCharPos(0x1, row * WIDTH + col, getColor(col, row));
+      for (int col = 0; col < WIDTH; col++)
+      {
+        printCharPos(0x1, row * WIDTH + col, getColor(col, row, round));
+      }
     }
   }
+
   while (1)
   {
   }
 }
 
-int getColor(int x, int y)
+int getColor(int x, int y, int round)
 {
   float r = (float)x / WIDTH;
   float g = (float)y / HEIGHT;
-  float b = 0;
+  float b = (float)round / 256;
 
   int ir = (int)(r * 256);
   int ig = (int)(g * 256);
