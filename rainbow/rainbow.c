ec6962493417a5cc4d00bed8464347448e69e5ad
Nate Wagner <nate.o.wagner@gmail.com>
2025-02-08 16:18:07 -0500

    feat: rainbow recompile

--- rainbow/rainbow.c
+++ rainbow/rainbow.c
@@ -11,28 +11,28 @@ static int HEIGHT = 60;
 
 int main()
 {
-  int nums[] = {0, 1, 2, 3, 4};
-  int *memNums = malloc(5 * sizeof(int));
+  // int nums[] = {0, 1, 2, 3, 4};
+  // int *memNums = malloc(5 * sizeof(int));
 
-  memcpy(memNums, nums, 5 * sizeof(int));
+  // memcpy(memNums, nums, 5 * sizeof(int));
 
-  for (size_t i = 0; i < 5; i++)
-  {
-    printInt(memNums[i], 0xffffff);
-  }
-
-  // int round = 0;
-  // while (1)
+  // for (size_t i = 0; i < 5; i++)
   // {
-  //   for (int row = 0; row < HEIGHT; row++)
-  //   {
-  //     for (int col = 0; col < WIDTH; col++)
-  //     {
-  //       printCharPos(0x1, row * WIDTH + col, getColor(col, row, round));
-  //     }
-  //   }
-  //   round = (round + 1) % 256;
+  //   printInt(memNums[i], 0xffffff);
   // }
+
+  int round = 0;
+  while (1)
+  {
+    for (int row = 0; row < HEIGHT; row++)
+    {
+      for (int col = 0; col < WIDTH; col++)
+      {
+        printCharPos(0x1, row * WIDTH + col, getColor(col, row, round));
+      }
+    }
+    round = (round + 1) % 256;
+  }
 }
 
 int getColor(int x, int y, int round)
