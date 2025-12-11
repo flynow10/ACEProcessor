b6bf32a80b6983da0bd142f43d8b3ea482dbcb61
Natalie Wagner <nate.o.wagner@gmail.com>
2025-12-11 01:06:08 -0500

    fix: card placed on first line after print

--- blackjack/card.c
+++ blackjack/card.c
@@ -157,5 +157,5 @@ void print_card(Card *card)
   printChar('|');
   setCurrentPos(lineStart * 80 + linePosStart + 80 * line++);
   printString("+=====+", 0xffffff);
-  setCurrentPos((linePosStart / 80) + (getCurrentPos() % 80));
+  setCurrentPos(lineStart + (getCurrentPos() % 80));
 }
